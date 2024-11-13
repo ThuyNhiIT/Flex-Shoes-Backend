cbd215746ec0708aa68205c7a19f957b7a2a8d50
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2024-11-13 15:44:44 +0700

    done BE productDetail

---
+++ src/main/java/com/flexshose/flexshoesbackend/dto/ColorDto.java
@@ -0,0 +1,13 @@
+package com.flexshose.flexshoesbackend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ColorDto {
+    private Integer colorId;
+    private String colorName;
+}
---
+++ src/main/java/com/flexshose/flexshoesbackend/service/ProductDetailService.java
@@ -0,0 +1,7 @@
+package com.flexshose.flexshoesbackend.service;
+
+import com.flexshose.flexshoesbackend.dto.ProductDetailDto;
+
+public interface ProductDetailService {
+    ProductDetailDto getProductDetailById(int id);
+}
